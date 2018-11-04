d22a0975556581b5c8dbe51ae2fd3d18426842b3
Brian Hyland <39333309+hylbrian@users.noreply.github.com>
2018-11-04 10:37:24 +0000

    Food item View plus code stubs

--- app/src/main/java/ie/ul/brianhyland/foodrater/FoodAdapter.java
+++ app/src/main/java/ie/ul/brianhyland/foodrater/FoodAdapter.java
@@ -1,4 +1,46 @@
 package ie.ul.brianhyland.foodrater;
 
-public class FoodAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.FoodViewHolder> {
+
+    public void addFood(){
+
+    }
+
+    @NonNull
+    @Override
+    public FoodViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FoodViewHolder foodViewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class FoodViewHolder extends RecyclerView.ViewHolder{
+
+
+        private ImageView mImageView;
+        private TextView mName;
+        private RatingBar mRatingBar;
+        public FoodViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mImageView = itemView.findViewById(R.id.food_pic);
+            mName = itemView.findViewById(R.id.name);
+            mRatingBar = itemView.findViewById(R.id.rating_bar);
+        }
+    }
 }
